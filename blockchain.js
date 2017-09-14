5f69cd39793ef47b0a3f4fc98170f93ab28d445b
Amedeo Spagnolo <amedeo.spagnolo@gmail.com>
2017-09-14 20:17:50 +0800

    start chain

--- blockchain.js
+++ blockchain.js
@@ -4,16 +4,27 @@ class Blockchain {
   }
 
   createGenesisBlock(){
-    return new Block(0, "01/01/2017", "Genesis Block", "0")
+    return new Block(0, 1505389735691, "Genesis Block", "0")
   }
 
   getLatestBlock(){
-    return this.chain[this.chain.lenght - 1]
+    return this.chain[this.chain.length - 1]
+  }
+
+  getLatestIndex(){
+    return this.chain[this.chain.length - 1].index
+  }
+
+  isValid(data){
+    console.log("data: " + data);
+    return true
   }
 
   addBlock(newBlock){
-    newBlock.previousHash = this.getLatestBlock().hash()
+    newBlock.previousHash = this.getLatestBlock().hash
     newBlock.hash = newBlock.calculateHash()
-    this.chain.push(newBlock)
+    if(this.isValid(newBlock)){
+      this.chain.push(newBlock)
+    }
   }
 }
--- main.js
+++ main.js
@@ -1,9 +1,16 @@
 let ledger = new Blockchain()
 
-users = ["AWE", "Bob", "Charlie", "Dan"]
+var tickets = ["ticket_1", "ticket_2", "ticket_3", "ticket_4", "ticket_5", "ticket_6", "ticket_7", "ticket_8", "ticket_9", "ticket_10", "ticket_11", "ticket_12", "ticket_13", "ticket_14", "ticket_15", "ticket_16", "ticket_17", "ticket_18", "ticket_19", "ticket_20"]
 
-function addNewBlock(index, time, data){
-  ledger.addBlock(new Block(index, time, data))
+var users = ["Alice", "Bob", "Charlie", "Dan"]
+
+function addNewBlock(data){
+    var newIndex = ledger.getLatestIndex() + 1
+    var newTime = new Date().getTime()
+    ledger.addBlock(new Block(newIndex, newTime, data))
 }
 
-console.log(ledger)
+
+// init AWE tickets
+addNewBlock({"AWE": tickets})
+console.log(ledger.chain)
